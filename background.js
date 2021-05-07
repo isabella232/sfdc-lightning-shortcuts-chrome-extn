ed47ba3ce22355d5c8fc34804706bf716046a32b
Rohit Macherla <rohit.macherla2@sky.uk>
2021-05-07 20:09:18 +0100

    fix: enabling after page url changes

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
+const isSalesforceRegex = /(\.force\.com)|(\.salesforce\.com)/;
+
+// Enable extension only for *.salesforce.com 
+// or *.lightning.force.com URLs  
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.onActivated.addListener(async info => {
     const tab = await chrome.tabs.get(info.tabId);
-    
-    // Match only *.salesforce.com or *.lightning.force.com URLs
-    const isSalesforceRegex = /(\.force\.com)|(\.salesforce\.com)/;
-    const isSalesforce = isSalesforceRegex.test(tab.url) 
-      ? chrome.action.enable(tab.tabId) 
-      : chrome.action.disable(tab.tabId);
+    enableTab(tab.tabId, tab.url);
+  });
+
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  	const tabURL = changeInfo.url;
+  	// Only enable/disable when url has changed
+  	if(tabURL) {
+	    enableTab(tabId, tabURL);
+	}
   });
 });
+
+function enableTab(tabId, tabURL) {
+	const isSalesforce = isSalesforceRegex.test(tabURL)
+      ? chrome.action.enable(tabId) 
+      : chrome.action.disable(tabId);
+}
