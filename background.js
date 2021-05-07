79fe2f300bdff9d756c24413d712452a21f9af3b
Rohit Macherla <rohit.macherla2@sky.uk>
2021-05-07 19:39:10 +0100

    feat: only enable in salesforce pages

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.onActivated.addListener(async info => {
     const tab = await chrome.tabs.get(info.tabId);
     
     // Match only *.salesforce.com or *.lightning.force.com URLs
-    const isSalesforceRegex = /(\.force\.com)|(\.salesforce.com)/;
-    const isGithub = isSalesforceRegex.test(tab.url) 
+    const isSalesforceRegex = /(\.force\.com)|(\.salesforce\.com)/;
+    const isSalesforce = isSalesforceRegex.test(tab.url) 
       ? chrome.action.enable(tab.tabId) 
       : chrome.action.disable(tab.tabId);
   });
